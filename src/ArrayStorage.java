a4c6649932b628bfc3688c81da69dfa1a7a1754f
Vladislav Brusov <gensu173@gmail.com>
2020-09-29 10:33:39 +0200

    correct HW_1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -8,7 +8,7 @@ public class ArrayStorage {
     int index = 0;
 
     void clear() {
-        Arrays.fill(storage, null);
+        Arrays.fill(storage, 0, index, null);
     }
 
     void save(Resume resume) {
@@ -17,37 +17,32 @@ public class ArrayStorage {
     }
 
     Resume get(String uuid) {
-        for (index = 0; index == size(); index++) {
-            if (storage[index].uuid.equals(uuid)) {
+        for (int i = 0; i == index; i++) {
+            if (storage[i].uuid.equals(uuid)) {
                 break;
             }
+            index = i;
         }
         return storage[index];
     }
 
     void delete(String uuid) {
-        for (index = 0; index < storage.length; index++) {
-            if (storage[index].uuid.equals(uuid)) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                System.arraycopy(storage, i + 1, storage, i, storage.length - 1 - index);
                 break;
             }
         }
-        if (storage.length - 1 - index >= 0)
-            System.arraycopy(storage, index + 1, storage, index, storage.length - 1 - index);
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOf(storage, size());
+        return Arrays.copyOf(storage, index);
     }
 
     int size() {
-        for (index = 0; index < storage.length; index++) {
-            if (storage[index] == null) {
-                break;
-            }
-        }
         return index;
     }
 }
