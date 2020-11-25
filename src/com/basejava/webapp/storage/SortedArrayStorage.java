39cadb362c25b1bdedeb0ef16d5271ccf53c005c
vbrusov <genu173@gmail.com>
2020-11-25 12:23:08 +0200

    edit HW_4

--- src/com/basejava/webapp/storage/SortedArrayStorage.java
+++ src/com/basejava/webapp/storage/SortedArrayStorage.java
@@ -10,7 +10,7 @@ public class SortedArrayStorage extends AbstractArrayStorage {
     protected void replaceDeletedResume(int index) {
         int numMoved = size - index - 1;
         if (numMoved > 0) {
-            System.arraycopy(storage, index - 1, storage, index, numMoved);
+            System.arraycopy(storage, index + 1, storage, index, numMoved);
         }
     }
 
@@ -18,6 +18,7 @@ public class SortedArrayStorage extends AbstractArrayStorage {
     protected void insertResume(Resume resume, int index) {
         int insertIndex = -index - 1;
         System.arraycopy(storage, insertIndex, storage, insertIndex + 1, size - insertIndex);
+        storage[insertIndex] = resume;
     }
 
     @Override
